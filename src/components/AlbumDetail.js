ff0fb08a0deec30ab370ee104fd16e69893097a9
C109156201 <C109156201@nkust.edu.tw>
2022-04-11 16:34:48 +0800

    homepage img

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -7,32 +7,28 @@ const AlbumDetail = ({ album, navigation }) => {
       marginX={1} marginBottom={2} borderRadius={3} shadow={2}
       _dark={{ borderColor: 'blueGray.500', borderWidth: 0.6 }}
     >
-      <HStack 
-        _dark={{ bg: "#2B3A61"}}
-        _light={{ bg: "white" }}>
-        <AspectRatio w="50" ratio={1}>
-          <Image
-            margin="1"
-            source={{ uri: album.thumbnail_image }}
-            alt="artist"
-          />
-        </AspectRatio>
-        <VStack paddingLeft={2} justifyContent="space-around">
-          <Text>{album.title}</Text>
-          <Text>{album.artist}</Text>
-        </VStack>
-      </HStack>
+      
       <Box p={1} _dark={{ bg: "#2B3A61" }}
         _light={{ bg: "white" }}>
         <Pressable 
           onPress={() => navigation.navigate('Detail', album)}
         >
-          <AspectRatio w="100%" ratio={1}>
-            <Image
-              source={{ uri: album.image }}
-              alt="album"
-            />            
-          </AspectRatio>
+          <HStack 
+            _dark={{ bg: "#2B3A61"}}
+            _light={{ bg: "white" }}>
+            <AspectRatio w="50" ratio={1}>
+              <Image
+                margin="1"
+                source={{ uri: album.thumbnail_image }}
+                alt="artist"
+              />
+            </AspectRatio>
+            <VStack paddingLeft={2} justifyContent="space-around">
+              <Text>{album.title}</Text>
+              <Text>{album.artist}</Text>
+            </VStack>
+          </HStack>
+          
         </Pressable>
       </Box>   
     </Box>
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -11,6 +11,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
+
 import AlbumScreen from '../screens/AlbumScreen';
 import DetailScreen from '../screens/DetailScreen';
 import SettingsScreen from '../screens/SettingsScreen';
@@ -23,6 +24,7 @@ import DisplaySettingScreen from '../screens/DisplaySettingScreen';
 import MyTheme from '../Theme';
 
 import albumData from "../json/albums.json";
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -53,14 +55,16 @@ const Navigation = () => {
   );
 }
 
-const MyDrawer = ({colorMode}) => {
+const MyDrawer = () => {
+  const { colorMode } = useColorMode();
   return (
     <Drawer.Navigator initialRouteName="HomeStack"  
         useLegacyImplementation
         screenOptions={{
           drawerStyle: {
-            backgroundColor: "light" ? "#FFE7AB" : "#2B3A61"
+            backgroundColor: colorMode == 'light' ? '#FFE7AB' : '#2B3A61',
           },
+          drawerActiveTintColor:colorMode == 'light' ? '#2B3A61' : '#FFE7AB',
         }}>
       <Drawer.Screen 
         name="HomeStack" 
@@ -70,8 +74,8 @@ const MyDrawer = ({colorMode}) => {
           headerShown: false,
           title: "登入",
           
-          drawerIcon: ({ colorMode }) => (
-            <FontAwesome5 name="user-astronaut" color={colorMode == "light" ? "#FFE7AB" : "#2B3A61"} size={20} />
+          drawerIcon: ({color}) => (
+            <FontAwesome5 name="user-astronaut" color={color} size={20} />
           ),
         }}
       />
@@ -81,8 +85,8 @@ const MyDrawer = ({colorMode}) => {
         options={{
           headerShown: false,
           title: "探索設定",
-          drawerIcon: ({ colorMode }) => (
-            <Ionicons name="md-settings-outline" color={colorMode == "light" ? "#FFE7AB" : "#2B3A61"} size={20} />
+          drawerIcon: ({color}) => (
+            <Ionicons name="md-settings-outline" color={color} size={20} />
           ),
         }}
       />
@@ -93,8 +97,8 @@ const MyDrawer = ({colorMode}) => {
           headerShown: false,
           title: "待複習星知",
           
-          drawerIcon: ({ colorMode }) => (
-            <FontAwesome5 name="star" color={colorMode == "light" ? "#FFE7AB" : "#2B3A61"} size={20} />
+          drawerIcon: ({color}) => (
+            <FontAwesome5 name="star" color={color} size={20} />
           ),
         }}
       />
@@ -104,8 +108,8 @@ const MyDrawer = ({colorMode}) => {
         options={{
           headerShown: false,
           title: "探索說明書",
-          drawerIcon: ({ colorMode }) => (
-            <AntDesign name="book" color={colorMode == "light" ? "#FFE7AB" : "#2B3A61"} size={20} />
+          drawerIcon: ({color}) => (
+            <AntDesign name="book" color={color} size={20} />
           ),
         }}
       />
@@ -121,8 +125,8 @@ const MyTabs = () => {
     <Tab.Navigator
       initialRouteName="HomeStack"
       screenOptions={{
-        tabBarInactiveTintColor: colorMode == 'light' ? colors.light500 : 'gray',
-        tabBarActiveTintColor: colorMode == 'light' ? colors.primary700 : 'white',
+        tabBarInActiveTintColor: colorMode == 'light' ? '#FFFFFF' : '#2B3A61',
+        tabBarActiveTintColor: colorMode == 'light' ? '#FFE7AB' : '#2B3A61',
         tabBarStyle: { backgroundColor: colorMode == 'light' ? '#2B3A61' : '#FFE7AB' },
         // headerShown: false
       }}
@@ -133,8 +137,9 @@ const MyTabs = () => {
         options={{
           headerShown: false,
           title: "Home",
-          tabBarIcon: ({ colorMode }) => (
-            <MaterialCommunityIcons name="home" color={colorMode == "light" ? "#2B3A61" : "#FFE7AB"} size={26} />
+          fontSize:"25",
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="home" color={color}  size={27} />
           ),
         }}
       />
@@ -144,8 +149,8 @@ const MyTabs = () => {
         options={{
           headerShown: false,
           title: "Planet",
-          tabBarIcon: ({ colorMode }) => (
-            <Ionicons name="planet-outline" color={colorMode == "light" ? "#2B3A61" : "#FFE7AB"} size={26} />
+          tabBarIcon: ({color}) => (
+            <Ionicons name="planet-outline" color={color} size={27} />
           ),
         }}
       />
@@ -155,8 +160,8 @@ const MyTabs = () => {
         options={{
           headerShown: false,
           title: "uplode",
-          tabBarIcon: ({ colorMode }) => (
-            <Ionicons name="cloud-upload-outline" color={colorMode == "light" ? "#2B3A61" : "#FFE7AB"} size={26} />
+          tabBarIcon: ({color}) => (
+            <Ionicons name="cloud-upload-outline" color={color} size={27} />
           ),
         }}
       />
@@ -201,11 +206,23 @@ const SettingsStack = ({navigation}) => {
             backgroundColor: colorMode == 'light' ? '#FFE7AB' : '#2B3A61',
           },
           headerTintColor: colorMode == 'light' ? '#2B3A61' : '#FFE7AB',
+          
           headerTitleStyle: {
             color: colorMode == 'light' ? '#2B3A61' : '#FFE7AB',
             fontWeight: '400',
             fontSize: 20
           },
+          headerLeft: () => (
+            <Pressable>
+                <MaterialCommunityIcons 
+                name={'chevron-left'} 
+                color= {colorMode == 'light' ? '#2B3A61' : '#FFE7AB'}
+                size={30}
+                style={{marginRight: 20}}
+                onPress={() => { navigation.goBack();}}
+            />
+            </Pressable>
+              ),
         }}
       />
     </Stack.Navigator>
@@ -261,6 +278,17 @@ const HomeStack = ({navigation}) => {
             fontWeight: '400',
             fontSize: 20
           },
+          headerLeft: () => (
+            <Pressable>
+                <MaterialCommunityIcons 
+                name={'chevron-left'} 
+                color={'black'} 
+                size={30}
+                style={{marginRight: 20}}
+                onPress={ () => {navigation.goBack();}}
+            />
+            </Pressable>
+            ),
         })}
       />
     </Stack.Navigator>
